5fe5c11a8987617a84e21cd3a4cf8bca516da94a
John <john@ncub8.com>
2012-12-12 13:25:03 -0500

    capture changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,35 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home Page" do
-    it "has h1 correct" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
 
-    it "should have base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App')
-    end	
+  subject { page }
+
+  describe "Home Page" do
+    before{ visit root_path}
+
+    it {should have_selector('h1', :text => 'Sample App')}
+    it {should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App')}
+    	
   end
 
-    describe "Help Page" do
-      it "works! (now write some real specs)" do
-    	visit '/static_pages/help'
-    	page.should have_content('Help')
-      end	
-	  it "should have the title 'Help'" do
-	      visit '/static_pages/help'
-	      page.should have_selector('title', :text => '| Help')
-	    end
+  describe "Help Page" do
+    before{ visit help_path }
 
-    end
+    it {should have_content('Help')}
+    it {should have_selector('title', :text => '| Help')}
+	   
+  end
 
-    describe "About Page" do
-    	it "works, holy crap!" do
-    		visit '/static_pages/about'
-    		page.should have_content('About US')
-    	end
-    	it "should have the title 'About'" do
-	      visit '/static_pages/about'
-	      page.should have_selector('title', :text => '| About')
-	    end
-    end	
+  describe "About Page" do
+    before{ visit about_path }
+    	it {should have_content('About US')}
+    	it {should have_selector('title', :text => '| About')}
+	end	
 
-    describe "Contact Page" do
-    	it "Contact Page Works" do
-    		visit '/static_pages/contact'
-    		page.should have_content('Contact')
-    	end
-    	it "has the title Contact Us" do
-    	  visit '/static_pages/contact'
-    	  page.should have_selector('title', :text => '| Contact Us')
-    	end
+  describe "Contact Page" do
+    before{visit contact_path}
+    	it {should have_content('Contact')}
+     	it {should have_selector('title', :text => '| Contact Us')}
+    	
     end	  	
 end
