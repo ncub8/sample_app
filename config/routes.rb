9ff387288d1ee2d713f71bf7895a2779c1afe71a
John <john@ncub8.com>
2012-12-19 11:24:30 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
 
  root to: 'static_pages#home'
 
- match '/signup', to: 'static_pages#help'
+ match '/signup', to: 'users#new'
 
   match '/help', to: 'static_pages#help'
 
