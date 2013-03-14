3ea5e558e39adb7faea3d13dea04f5ab61b22fb3
John <john@ncub8.com>
2013-03-14 16:27:22 -0400

    try to get test driven development going

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,43 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home Page" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => '| Home')
-    end	
-  end
+	describe "Home Page" do
 
-    describe "Help Page" do
-      it "works! (now write some real specs)" do
-    	visit '/static_pages/help'
-    	page.should have_content('Help')
-      end	
-	  it "should have the title 'Help'" do
-	      visit '/static_pages/help'
-	      page.should have_selector('title', :text => '| Help')
-	    end
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			page.should have_content('Sample App')
+		end
 
-    end
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
+		end
+	end
 
-    describe "About Page" do
-    	it "works, holy crap!" do
-    		visit '/static_pages/about'
-    		page.should have_content('About US')
-    	end
-    	it "should have the title 'About'" do
-	      visit '/static_pages/about'
-	      page.should have_selector('title', :text => '| About')
-	    end
-    end	
+	describe "Help Page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			page.should have_content('Help')
+		end
 
-    describe "Contact Page" do
-    	it "Contact Page Works" do
-    		visit '/static_pages/contact'
-    		page.should have_content('Contact')
-    	end
-    	it "has the title Contact Us" do
-    	  visit '/static_pages/contact'
-    	  page.should have_selector('title', :text => '| Contact Us')
-    	end
-    end	  	
+		it "should have the right title" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
+		end
+	end
+
+	describe "About page" do
+		it "should have the content 'About Us" do
+			visit '/static_pages/about'
+			page.should have_content('About Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
+		end
+	end
+
+
 end
