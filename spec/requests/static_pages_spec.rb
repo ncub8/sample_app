d58326e1a67021c78cb3073a1ecacf48c092c673
John <john@ncub8.com>
2013-03-15 10:24:38 -0400

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
+			page.should have_selector('title', :text => 'Ruby on Rails Sample App | Home')
 		end
 	end
 
@@ -23,7 +23,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
+			page.should have_selector('title', :text => 'Ruby on Rails Sample App | Help')
 		end
 	end
 
@@ -35,7 +35,7 @@ describe "StaticPages" do
 
 		it "should have the right title" do
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
+			page.should have_selector('title', :text => 'Ruby on Rails Sample App | About Us')
 		end
 	end
 
